1a27e0d15086e4cc580a10956a1f4c3862b033dd
Nisarg Shah <nisargs@cs.wisc.edu>
2020-02-13 00:42:11 -0600

    Truncate file if key/value read fails on startup Update build to C++ 17 and enable filesystem support

--- examples/cpp/keyvaluestore/logstorage.h
+++ examples/cpp/keyvaluestore/logstorage.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <experimental/filesystem>
 #include <fstream>
 #include <memory>
 #include <map>
@@ -12,16 +13,18 @@ class LogStorage/*: public Persistence*/ {
 private:
     std::shared_ptr<std::ofstream> outLog;
     std::shared_ptr<std::ifstream> inLog;
+    std::experimental::filesystem::path logPath;
 
 public:
-    LogStorage(const std::string& fileName) {
-        //checkInit(fileName);
+    LogStorage(const std::string& filePath) {
+        //checkInit(filePath);
 
+        logPath = filePath;
         outLog = std::shared_ptr<std::ofstream>(new std::ofstream (
-                    fileName,
+                    filePath,
                     std::ofstream::out | std::ofstream::app | std::ofstream::binary));
         inLog = std::shared_ptr<std::ifstream>(new std::ifstream (
-                    fileName, std::ifstream::in | std::ofstream::binary));
+                    filePath, std::ifstream::in | std::ofstream::binary));
         assert(outLog->good());
         assert(inLog->good());
     }
@@ -30,8 +33,8 @@ public:
      * On first run, reserve 2G space on disk for log file
      */
     /*
-    void checkInit(const std::string& fileName) {
-        std::ifstream f(fileName);
+    void checkInit(const std::string& filePath) {
+        std::ifstream f(filePath);
         if (f.good()) return;
 
         std::cout << "Running for the first time, initializing log..." << std::endl;
@@ -40,7 +43,7 @@ public:
         char *max_buf = new char [max_size];
         memset(max_buf, '\0', sizeof(char) * max_size);
 
-        std::ofstream of (fileName, std::ofstream::out | std::ofstream::binary);
+        std::ofstream of (filePath, std::ofstream::out | std::ofstream::binary);
         of.write(max_buf, sizeof(char) * max_size);
         of.flush();
         std::cout << "Initialized log successfully" << std::endl;
@@ -65,15 +68,6 @@ public:
     }
 
     /*
-    char * read(const std::string& key, const uint64_t& offset, const uint64_t& size) {
-        char *buffer = new char [size];
-        inLog.seekg(offset, inLog.beg);
-        inLog.read(buffer, size);
-        return buffer;
-    }
-    */
-
-    /*
      * Read all key-value pairs from disk
      * Used to re-construct map in memory after crash
      */
@@ -83,6 +77,10 @@ public:
         // Seek to beginning if not already there
         inLog->seekg(0, inLog->beg);
 
+        // Marks offset until which the contents of log are consistent and not corrupt
+        uint64_t consistentOffset = 0;
+        bool corruptBytes = false;
+
         while (!inLog->eof()) {
 
             // Read key and value size
@@ -90,11 +88,14 @@ public:
             size_t valueSize = 0;
 
             if (!(inLog->read((char *)(&keySize), sizeof(size_t)))) {
+                // TODO: Currently this will occur on every startup
                 std::cerr << "Read key size failed!\n";
+                corruptBytes = true;
                 break;
             }
             if (!(inLog->read((char *)(&valueSize), sizeof(size_t)))) {
-                std::cerr << "Read key size failed!\n";
+                std::cerr << "Read value size failed!\n";
+                corruptBytes = true;
                 break;
             }
 
@@ -107,14 +108,18 @@ public:
 
             if (!(inLog->read(key, keySize))) {
                 std::cerr << "Read key size failed!\n";
+                corruptBytes = true;
                 break;
             }
             key[keySize] = 0; // read does not add null character at the end
             if (!(inLog->read(value, valueSize))) {
                 std::cerr << "Read key size failed!\n";
+                corruptBytes = true;
                 break;
             }
             value[valueSize] = 0;
+
+            consistentOffset += 2*sizeof(size_t) + keySize + valueSize;
 
             std::cout << "key: " << key
                       << ", value: " << value << std::endl;
@@ -126,6 +131,11 @@ public:
             kvStore.insert(a, keyStr);
             a->second = {keyStr, std::string(value)};
             a.release();
+        }
+
+        if (corruptBytes) {
+            std::cerr << "Truncating log to " << consistentOffset << " bytes\n";
+            std::experimental::filesystem::resize_file(logPath, consistentOffset);
         }
     }
 
